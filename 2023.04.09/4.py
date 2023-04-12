3b16fc98c515c9a946ca4de4c53f7e2cc1958a78
Gennadiy <renardes@gmail.com>
2023-04-12 19:43:09 +0500

    2023.04.09: комментарии, правки, итоги

--- 2023.04.09/4.py
+++ 2023.04.09/4.py
@@ -1,13 +1,19 @@
-number = int (input())
+number = int(input())
 
+# КОММЕНТАРИЙ: цифра — digit, число — number, num
 first_num = number // 100
-second_num = (number - first_num*100)//10
+second_num = (number - first_num*100) // 10
 third_num = number % 10
 
 sum_of_num = first_num + second_num + third_num
+# КОММЕНТАРИЙ: произведение — product, факториал — factorial
 factorial = first_num * second_num * third_num
 
-print (f'Сумма цифр = {sum_of_num} \nПроизведение цифр = {factorial}')
+print(f'Сумма цифр = {sum_of_num} \n'
+      f'Произведение цифр = {factorial}')
 
 # Сумма цифр = 15
 # Произведение цифр = 125
+
+
+# ИТОГ: отлично — 4/4
--- 2023.04.09/5.py
+++ 2023.04.09/5.py
@@ -1,8 +1,12 @@
-first_num = int (input ())
-second_num = int (input ())
+first_num = int(input())
+second_num = int(input())
 
 miles = float(f'{first_num}.{second_num}')
 
-print (f'{miles} миль = {round(miles*1.61,1)} км')
+# ИСПРАВИТЬ: если результат округления должен быть использован где-то ещё, тогда используете функцию round(); в данном случае выгоднее воспользоваться синтаксисом f-строк
+print(f'{miles} миль = {round(miles*1.61,1)} км')
 
 # 56.3 миль = 90.6 км
+
+
+# ИТОГ: отлично — 4/4
