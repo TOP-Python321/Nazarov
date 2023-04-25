57537b446aa2bae9bfecde1232bbb69ac90e2c7d
Roman-Nazarov <romannazarov@mail.ru>
2023-04-25 13:38:46 +0300

    исправил, согласно комментариям

--- 2023.04.16/2.py
+++ 2023.04.16/2.py
@@ -1,17 +1,17 @@
-divisible = int(input())
-divisor = int(input())
+# divisible = int(input())
+# divisor = int(input())
 
 # СДЕЛАТЬ: подумайте и перепишите код так, чтобы обойтись только одним блоком if
 
-if divisible % divisor == 0:
-    print(f'{divisible} делится на {divisor} нацело \n'
+# if divisible % divisor == 0:
+    # print(f'{divisible} делится на {divisor} нацело \n'
           # КОММЕНТАРИЙ: круглые скобки не нужны, их можно опустить
-          f'частное: {(divisible // divisor)}')
-else:
-    print(f'{divisible} не делится на {divisor} нацело \n'
-          f'неполное частное: {divisible // divisor} \n'
+          # f'частное: {(divisible // divisor)}')
+# else:
+    # print(f'{divisible} не делится на {divisor} нацело \n'
+          # f'неполное частное: {divisible // divisor} \n'
           # ИСПРАВИТЬ: вы уже вычисляли остаток, а здесь повторно выполняете ту же операцию — неоптимально
-          f'остаток: {divisible % divisor}')
+          # f'остаток: {divisible % divisor}')
 
 
 # 8 делится на 2 нацело
@@ -23,3 +23,27 @@ else:
 
 
 # ИТОГ: хорошо — 2/3
+
+# Переписал полностью, с одним блоком if и двумя новыми переменными, позволяющими сократить количество вычилений
+
+divisible = int(input())
+divisor = int(input())
+res_of_div = int(divisible // divisor)
+remainder = int(divisible % divisor)
+
+answer = (f'{divisible} делится на {divisor} нацело \n' 
+          f'частное: {res_of_div}')   
+ 
+if remainder:
+    answer = (f'{divisible} не делится на {divisor} нацело \n' 
+             f'неполное частное: {res_of_div} \n'
+             f'остаток: {remainder}')
+
+print(answer)
+
+# 50 делится на 5 нацело
+# частное: 10
+
+# 7 не делится на 3 нацело
+# неполное частное: 2
+# остаток: 1
--- 2023.04.16/4.py
+++ 2023.04.16/4.py
@@ -1,26 +1,36 @@
 # КОММЕНТАРИЙ: клетка (для животных) — cage; клетка (часть разметки, ячейка) — cell; клетка (игрового поля) — square, field, cell
 # КОММЕНТАРИЙ: один (количественное числительное) — one; первый (порядковое числ.) — first; два (кол. числ.) — two; второй (пор. числ.) — second
-cage_one = input()
-cage_second = input()
+
+# Заменил имена переменных на рекомендованные. По поводу cage - меня сбил google переводчик, 
+# ввел туда "шахматная клетка" в поиске дополнительного синонима через алгоритмы переводчика,
+# в это время свой мозг выключился, понадеявшись на ИИ )))). Конечно же field или square здесь подходят.
+# и конечно же, cage это металлическая клетка, видимо, ИИ предположил запереть в ней шахматные фигуры или доску )))
+
+square_first = input()
+square_second = input()
 
 # КОММЕНТАРИЙ: здесь подойдёт odd_verticals (нечётные_вертикали)
-arr = ['a', 'c', 'e', 'g'] 
+odd_verticals = ['a', 'c', 'e', 'g'] 
 
-if cage_one[0] in arr:
-    num_1 = 0
-else:
-    num_1 = 1
+# if square_first[0] in odd_verticals:
+    # num_1 = 0
+# else:
+    # num_1 = 1
 
-if cage_second[0] in arr:
-    num_2 = 0
-else:
-    num_2 = 1
+# if square_second[0] in odd_verticals:
+    # num_2 = 0
+# else:
+    # num_2 = 1
 
 # ИСПОЛЬЗОВАТЬ: а ещё можно вспомнить про преобразование типов:
 # num_1 = int(cage_one[0] not in arr)
 # num_2 = int(cage_second[0] not in arr)
 
-if (num_1 + int(cage_one[1]) + num_2 + int(cage_second[1])) % 2 == 0:
+# Использовал рекомендованное, действительно, здорово получилось: булевы значения с приведением к int заменили аж две ветки if-else
+num_1 = int(square_first[0] not in odd_verticals)
+num_2 = int(square_second[0] not in odd_verticals)
+
+if (num_1 + int(square_first[1]) + num_2 + int(square_second[1])) % 2 == 0:
     print('да')
 else:
     print('нет')
@@ -38,7 +48,7 @@ else:
 # попробовал сделать без использования функции ord(), через объявление списка (list)
 # ord() использовал в шестой задаче, всё в тренировочных целях, понятно, что с ord()
 # получилось бы здесь короче, примерно вот так:
-# if ((ord(cage_one[0]) + int(cage_one[1])) % 2 == (ord(cage_second[0] + int(cage_second[1])) % 2):
+# if ((ord(square_one[0]) + int(square_one[1])) % 2 == (ord(square_second[0] + int(square_second[1])) % 2):
 #     print('да')
 # else:
 #     print('нет')
