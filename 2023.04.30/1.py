9e781f58c934bbc99680abf31417a2261ad4080d
Roman-Nazarov <romannazarov@mail.ru>
2023-05-13 09:19:10 +0300

    HW

--- 2023.04.30/1.py
+++ 2023.04.30/1.py
@@ -1,6 +1,7 @@
 email = input("Введите адрес электронной почты: ")
 
 if email[0] != '@' \
+    and email.count('@') > 0 \
     and email.count('.') > 0 \
     and email.rfind('.') > email.find('@'):
     print("Да")
