96fb84ad81ba31abb1907a518375166b99ca0932
Roman-Nazarov <romannazarov@mail.ru>
2023-05-19 21:21:13 +0300

    HW

---
+++ 2023.04.30/8.py
@@ -0,0 +1,25 @@
+names_cin = sorted(input('Введите имена файлов: ').split('; '))
+original_names = {}
+names_cout = []
+
+for i in names_cin:
+    original_names[i] = original_names.get(i, 0)+1
+    
+    if original_names[i] == 1:
+        names_cout.append(i)        
+    else:
+        names_cout.append(i[:i.index('.')] + '_' + str(original_names[i]) + i[i.index('.'):])
+        
+print(*names_cout, sep='\n')
+
+# Введите имена файлов: 1.py; 1.py; src.tar.gz; aux.h; main.cpp; functions.h; main.cpp; 1.py; main.cpp; src.tar.gz
+# 1.py
+# 1_2.py
+# 1_3.py
+# aux.h
+# functions.h
+# main.cpp
+# main_2.cpp
+# main_3.cpp
+# src.tar.gz
+# src_2.tar.gz
